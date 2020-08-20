a89adefb5014a78639bf345082271caf143f23b7
David Cui <fordavidemail@gmail.com>
2020-08-20 10:36:45 -0400

    first commit!

---
+++ forjenkins/src/main/java/com/example/forjenkins/ForjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.forjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForjenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForjenkinsApplication.class, args);
+    }
+
+}
